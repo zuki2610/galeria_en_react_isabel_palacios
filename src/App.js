134b1c30ea5d567e40c8e912a7e9db9490858bb9
Isabel Palacios <issabelpalacios@gmail.com>
2023-01-15 23:45:46 -0300

    FIX: navegacion con react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from './Componentes/Menu';
 import Footer from './Componentes/Footer';
 import Productos from './Componentes/Produtos';
@@ -9,13 +9,12 @@ import Home from './Componentes/Home';
 
 function App() {
   return (
-  <BrowserRouter>
+  <BrowserRouter >
   <Menu />
   <main>
     <Routes>
         <Route path='/galeria_en_react_isabel_palacios/productos' element={<Productos />} />
         <Route path='/galeria_en_react_isabel_palacios/' element={<Home />} />
-        <Route path='/' element={<Home />} />
     </Routes>
   </main>
   <Footer /> 
--- src/componentes/menu.jsx
+++ src/componentes/menu.jsx
@@ -3,18 +3,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import { LinkContainer } from 'react-router-bootstrap'
 
 const Menu = () => {
 return (
     <>
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand to="/galeria_en_react_isabel_palacios/">Toy Store</Navbar.Brand>
+        <LinkContainer to="/galeria_en_react_isabel_palacios/">
+            <Navbar.Brand>Toy Store</Navbar.Brand>
+        </LinkContainer>
         <Nav className="me-auto">
-          <Nav.Link to="/">Home</Nav.Link>
-          <Nav.Link to="/galeria_en_react_isabel_palacios/productos">Productos</Nav.Link>
-          <Nav.Link href="#footer">About Us</Nav.Link>
+        <LinkContainer to="/galeria_en_react_isabel_palacios/">
+          <Nav.Link>Home</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/galeria_en_react_isabel_palacios/productos">
+          <Nav.Link>Productos</Nav.Link>
+        </LinkContainer>
+        <Nav.Link href="#footer">About Us</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
