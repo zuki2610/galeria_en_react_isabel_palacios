929ff30e34b631520829d63418d1cf194b671530
Isabel Palacios <issabelpalacios@gmail.com>
2023-01-18 22:19:48 -0300

    FIX: delete assets

--- src/componentes/Produtos.jsx
+++ src/componentes/Produtos.jsx
@@ -5,6 +5,7 @@ import Cards from './Cards';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+
 function Productos() {
     const contenido =   [
       { 
