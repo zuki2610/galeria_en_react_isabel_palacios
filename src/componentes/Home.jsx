2fea5dadd29d6a02fa66fe65311905033066727c
Isabel Palacios <issabelpalacios@gmail.com>
2023-01-22 20:52:18 -0300

    fix: css

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -12,10 +12,10 @@ return (
     <>
     <Menu isLogin={false}/>
      <div className="home App bg-dark p-5">
-    
+    <div className="marvel">
     <Header titulo="Donde encontrarás los mejores juguetes y accesorios de Marvel Comics para ti"/>
     <Image className="p-5" src="./assets/img/Logo1.png"/>
-  
+    </div>
     <Footer /> 
     </div>
     </>
